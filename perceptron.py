4bf4a7ead7cbcbb747f45d9e56fc99e8c4dd1d73
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-04-19 16:28:37 -0300

    Modelando constructor e função para mostrar as estatísticas do perceptron. Adicionando exemplo

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,16 @@
+import random
+
 class Perceptron:
-    def __init__(self):
-        pass
+    def __init__(self, x):
+        self.w = random.random() # Peso
+        self.b = random.random() # Bias
+        self.line = self.w*x + self.b # Equação da Reta
+
+    def print_stats(self):
+        print(f"Peso (w) = {self.w:.2f}")
+        print(f"Bias (b) = {self.b:.2f}")
+        print(f"Eq. Reta (y = w*x + b) = {self.line:.2f}")
+
+# Inicializando perceptron com exemplo  
+p = Perceptron(10)
+p.print_stats()
