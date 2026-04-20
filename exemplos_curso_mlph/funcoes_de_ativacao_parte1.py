9734935dd6e6fdb1dbcb7d73eb245a19257952a6
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-04-20 14:50:49 -0300

    Adicionando comandos para armazenar os valores de x, do somatório e da função de ativação (modificando função calculate para retornar os valores do somatório e da função de ativação). Adicionando plotagem dos gráficos da reta e da função de ativação

--- exemplos_curso_mlph/funcoes_de_ativacao_parte1.py
+++ exemplos_curso_mlph/funcoes_de_ativacao_parte1.py
@@ -6,6 +6,7 @@ root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root)
 
 # Exemplo
+import matplotlib.pyplot as plt
 from ativacao.sigmoide import sigmoide
 
 class PerceptronFA1:
@@ -21,6 +22,8 @@ class PerceptronFA1:
         print(f"Resultado da Eq. Reta (y = {self.w}*{x} + {self.b}): {self.line}")
         print(f"Resultado da Função de Ativação (Sigmoide): {self.activation:.3f}")
 
+        return self.line, self.activation
+
     def print_stats(self):
         print(f"Peso (w) = {self.w}")
         print(f"Bias (b) = {self.b}")
@@ -29,5 +32,26 @@ class PerceptronFA1:
 p = PerceptronFA1()
 p.print_stats()
 
+# Calculando resultados a partir de valores de x
+sum_results = [x for x in range(-10, 11)]
+line_results = [] 
+activation_results = []
+
 for x in range(-10, 11):
-    p.calculate(x)
+    y, f = p.calculate(x)
+
+    line_results.append(y)
+    activation_results.append(f)
+
+# Plotando gráficos da funções - reta e ativação (sigmoide)
+plt.scatter(sum_results, line_results)
+plt.title("Equação da Reta")
+plt.xlabel("Valores de X")
+plt.ylabel("Somatório")
+plt.show()
+
+plt.scatter(line_results, activation_results)
+plt.title("Função Sigmoide")
+plt.xlabel("Somatório")
+plt.ylabel("Sigmoide")
+plt.show()
