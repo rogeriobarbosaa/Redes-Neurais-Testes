3a197aaedfd817adf71f8293c35fb4d3fd7532f6
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-04-20 00:35:40 -0300

    Adicionando função de ativação (Threshold) para completar o perceptron. Add gitignore

--- perceptron_completo.py
+++ perceptron_completo.py
@@ -1,10 +1,12 @@
 import random
+from ativacao.threshold import threshold
 
 class PerceptronCompleto:
     def __init__(self, x_vet):
         self.w_vet = [random.random() for _ in range(len(x_vet))]     # Pesos (vetor)
         self.b = random.random()                                      # Bias
         self.line = self.produto_vetorial(x_vet, self.w_vet) + self.b # Eq. Reta ou Somatório
+        self.activation = threshold(self.line)                        # Função de ativação de exemplo: Threshold
 
     def produto_vetorial(self, x_vet, w_vet):
         line = 0
@@ -18,6 +20,7 @@ class PerceptronCompleto:
         print(f"Pesos (w) = {pesos}")
         print(f"Bias (b) = {self.b:.3f}")
         print(f"Eq. Reta (y = w1*x1 + w2*x2 + ... + b) = {self.line:.3f}")
+        print(f"Resultado da função de ativação (Threshold) = {self.activation}")
 
 # Inicializando perceptron
 x_vet = input("Insira os valores de entrada: ").split(", ")
